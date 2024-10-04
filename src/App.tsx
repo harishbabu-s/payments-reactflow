b7a21e7c6af76d3318826c8a4db5663baff713c7
harishbabu-s <roboticonics@gmail.com>
2024-10-04 22:51:40 +0530

    React Flow Check

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ReactFlow } from '@xyflow/react';
 
-function App() {
+import '@xyflow/react/dist/style.css';
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '90vw', height: '90vh', border: '1px solid black', marginInline: '5vw', marginTop: '5vh' }}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} />
     </div>
   );
 }
-
-export default App;
