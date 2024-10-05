356b44e2b8f140246b7b6aeb42a286d94eefd349
harishbabu-s <roboticonics@gmail.com>
2024-10-05 13:17:11 +0530

    Tried

--- src/customNodes.jsx
+++ src/customNodes.jsx
@@ -1,4 +1,4 @@
-const initialNodes = [
-    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+export const initialNodes = [
+    { id: '1', position: { x: 0, y: 0 }, data: { label: 'Payment Options' } },
     { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
 ];
